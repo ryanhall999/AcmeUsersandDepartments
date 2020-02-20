703fbe2e3ec6df143af2e5c805be5db140f7d4eb
erika-hilty <erika.hity@gmail.com>
2020-02-19 21:25:14 -0500

    broken

--- server.js
+++ server.js
@@ -1,42 +1,50 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const db = require("./db");
-app.use(require("cors")());
+const db = require('./db');
+app.use(express.json());
+app.use(require('cors')());
 
 //don't forget your body parser!
 
-app.get("/api/departments", (req, res, next) => {
-	db.readDepartments()
-		.then(departments => res.send(departments))
-		.catch(next);
+app.get('/api/departments', (req, res, next) => {
+  db.readDepartments()
+    .then(departments => res.send(departments))
+    .catch(next);
 });
 
-app.get("/api/users", (req, res, next) => {
-	db.readUsers()
-		.then(users => res.send(users))
-		.catch(next);
+app.get('/api/users', (req, res, next) => {
+  db.readUsers()
+    .then(users => res.send(users))
+    .catch(next);
 });
 
-app.post("/api/users", (req, res, next) => {
-	console.log(req.params);
-	db.createUser()
-		.then(users => res.send(users))
-		.catch(next);
+app.post('/api/users', (req, res, next) => {
+  console.log(req.body.name);
+  db.createUser(req.body.name)
+    .then(db.readUsers())
+    .then(users => res.send(users))
+    .catch(next);
+});
+
+app.post('/api/departments', (req, res, next) => {
+  db.createDepartment(req.body.name)
+    .then(departments => res.send(departments))
+    .catch(next);
 });
 
 app.use((req, res, next) => {
-	next({ status: 404, message: `page not found - ${req.method} - ${req.url}` });
+  next({ status: 404, message: `page not found - ${req.method} - ${req.url}` });
 });
 
 app.use((err, req, res, next) => {
-	res
-		.status(err.status || 500)
-		.send({ error: err.message ? err.message : err.toString() });
+  res
+    .status(err.status || 500)
+    .send({ error: err.message ? err.message : err.toString() });
 });
 
 const port = process.env.PORT || 3000;
 
 db.sync().then(() => {
-	console.log("synced");
-	app.listen(port, () => console.log(`listening on port ${port}`));
+  console.log('synced');
+  app.listen(port, () => console.log(`listening on port ${port}`));
 });
